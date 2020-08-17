0d11c2366868e15b20f138c03c23f90ed04b4546
Joshua "jag" Ginsberg <jag@flowtheory.net>
2020-08-17 12:48:48 -0400

    Template nginx conf.

--- services/nginx/turnpike-entrypoint.sh
+++ services/nginx/turnpike-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+export VAR='$'
+envsubst < /etc/nginx/api_gateway.conf.tmpl > /etc/nginx/api_gateway.conf
 python3 /etc/nginx/build-backends.py $BACKENDS_CONFIG_MAP /etc/nginx/api_conf.d

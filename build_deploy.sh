ede9c4dc99165fcd1d3f5e5feeb0328bf4b453c6
Joshua "jag" Ginsberg <jag@flowtheory.net>
2020-08-19 10:45:10 -0400

    More Openshift work.

---
+++ build_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -exv
+
+NGINX_IMAGE="quay.io/cloudservices/turnpike-nginx"
+WEB_IMAGE="quay.io/cloudservices/turnpike-web"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+
+docker --config="$DOCKER_CONF" build -t "${NGINX_IMAGE}:${IMAGE_TAG}" services/nginx
+docker --config="$DOCKER_CONF" build -t "${WEB_IMAGE}:${IMAGE_TAG}" services/web
+
+docker --config="$DOCKER_CONF" push "${NGINX_IMAGE}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${WEB_IMAGE}:${IMAGE_TAG}"
---
+++ pr_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -exv
+
+python3 -m venv .
+source bin/activate
+bin/pip3 install --upgrade pip pipenv
+bin/pipenv install -d
+bin/pre-commit run -a

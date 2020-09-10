ab9931244cc114398a8b4df06da10d6febbd7b55
Kevin Howell <khowell@redhat.com>
2020-09-10 09:45:43 -0400

    Let X509AuthPlugin subclasses override headers

--- turnpike/plugins/x509.py
+++ turnpike/plugins/x509.py
@@ -1,23 +1,35 @@
 import logging
 from flask import request
 
-from ..config import HEADER_CERTAUTH_SUBJECT, HEADER_CERTAUTH_ISSUER
 from ..plugin import TurnpikeAuthPlugin
 
 logger = logging.getLogger(__name__)
 
 
 class X509AuthPlugin(TurnpikeAuthPlugin):
+    """
+    X509AuthPlugin performs authorization on headers that represent an X509
+    client certificate's identity. Subclasses may override the headers used
+    by setting the `subject_header` and `issuer_header` attributes.
+    """
     name = "X509"
     principal_type = "X509"
-    headers_needed = set([HEADER_CERTAUTH_SUBJECT, HEADER_CERTAUTH_ISSUER])
+
+    def __init__(self, app):
+        super().__init__(app)
+        self.subject_header = self.app.config['HEADER_CERTAUTH_SUBJECT']
+        self.issuer_header = self.app.config['HEADER_CERTAUTH_ISSUER']
+
+    @property
+    def headers_needed(self):
+        return set([self.subject_header, self.issuer_header])
 
     def process(self, context, backend_auth):
         logger.debug("Begin X509 plugin processing")
-        if "x509" in backend_auth and HEADER_CERTAUTH_SUBJECT in request.headers:
+        if "x509" in backend_auth and self.subject_header in request.headers:
             auth_data = dict(
-                subject_dn = request.headers[HEADER_CERTAUTH_SUBJECT],
-                issuer_dn = request.headers.get(HEADER_CERTAUTH_ISSUER),
+                subject_dn = request.headers[self.subject_header],
+                issuer_dn = request.headers.get(self.issuer_header),
             )
             logger.debug(f"X509 auth_data: {auth_data}")
             context.auth = dict(auth_data=auth_data, auth_plugin=self)

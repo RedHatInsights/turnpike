93eae77ebaf14a6f63cbfda3138f042d00e7c858
Keith Walsh <kwalsh@redhat.com>
2020-11-04 08:55:46 -0500

    Dynamically supply -nginx.scrape-uri

--- build_deploy.sh
+++ build_deploy.sh
@@ -18,7 +18,10 @@ docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 docker --config="$DOCKER_CONF" build -t "${NGINX_IMAGE}:${IMAGE_TAG}" nginx
 docker --config="$DOCKER_CONF" build -t "${WEB_IMAGE}:${IMAGE_TAG}" .
-docker --config="$DOCKER_CONF" build -f ./nginx/Dockerfile-prometheus -t "${NGINX_PROMETHEUS_IMAGE}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" build \
+       --build-arg scrapeuri=http://nginx:8888/stub_status \
+       -f ./nginx/Dockerfile-prometheus \
+       -t "${NGINX_PROMETHEUS_IMAGE}:${IMAGE_TAG}" .
 
 docker --config="$DOCKER_CONF" push "${NGINX_IMAGE}:${IMAGE_TAG}"
 docker --config="$DOCKER_CONF" push "${WEB_IMAGE}:${IMAGE_TAG}"

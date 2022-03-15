8f2a3ad0ac51d585918c84047d19dc36b87813bb
Casey Williams <caswilli@redhat.com>
2022-03-15 14:22:11 -0400

    Removing RPM dependencies from pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -2,9 +2,6 @@
 
 set -exv
 
-dnf config-manager --set-enable codeready-builder-for-rhel-8-x86_64-rpms
-dnf install -y libxml2-devel xmlsec1-devel xmlsec1-openssl-devel libtool-ltdl-devel
-
 python3 -m venv .
 source bin/activate
 bin/pip3 install --upgrade pip pipenv

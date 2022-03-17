38be213e81e83f20a2fa073ebe5b47c89216fa01
Casey Williams <caswilli@redhat.com>
2022-03-17 09:53:49 -0400

    Re-adding Python Wheel to  pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -2,12 +2,13 @@
 
 set -exv
 
-export LC_ALL=en_US.utf-8
-export LANG=en_US.utf-8
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
 
 python3 -m venv .
 source bin/activate
 bin/pip3 install --upgrade pip pipenv
+bin/pipenv install wheel
 bin/pipenv install -d
 bin/pre-commit run -a
 bin/pytest

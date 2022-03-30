d21b5da70f9c25471249105c9f7be9fc28854ca7
Casey Williams <caswilli@redhat.com>
2022-03-30 15:50:57 -0400

    Pushing the new containerized PR_CHECK

--- pr_check.sh
+++ pr_check.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
-set -exv
+export CONTAINER_NAME="turnpike-pr-check"
+export IMAGE_TAG="turnpike:pr-check"
 
-python3 -m venv .
-source bin/activate
-bin/pip3 install --upgrade pip pipenv
-bin/pipenv install -d
-bin/pre-commit run -a
-bin/pytest
+function teardown_podman() {
+    podman rm -f $CONTAINER_NAME || true
+    podman image rm -f $IMAGE_TAG || true
+}
+
+# Catches process termination and cleans up Podman artifacts
+trap "teardown_podman" EXIT SIGINT SIGTERM
+
+set -ex
+
+# Setup environment for pre-commit check
+pip install black pre-commit
+
+# Run pre-commit
+if ! (pre-commit run -a); then
+    echo "pre-commit ecountered an issue"
+    exit 1
+fi
+
+# Build PR_CHECK Image
+podman build --no-cache -f Dockerfile-pr-check --tag $IMAGE_TAG
+
+# Build PR_Check Container
+podman create --name $CONTAINER_NAME $IMAGE_TAG
+
+# Run PR_CHECK Container (attached with standard output)
+# and reports if the Containerized PR_Check fails
+if ! (podman start -a $CONTAINER_NAME); then
+    echo "Test failure observed; aborting"
+    exit 1
+fi
+
+# Pass Jenkins dummy artifacts as it needs
+# an xml output to consider the job a success.
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
---
+++ pr_tests/pr_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+pip install --upgrade pip pipenv
+pipenv install -d
+pipenv run pytest --disable-pytest-warnings tests/
+result=$?
+
+if [ $result -ne 0 ]; then
+    echo '====================================='
+    echo '====  ✖  ERROR: PR_CHECK FAILED  ===='
+    echo '====================================='
+    exit 1
+fi

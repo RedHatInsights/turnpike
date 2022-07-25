05edf344941cc990e51353bc33248a0ba0dddbc9
Matt Holder <mjholder96@gmail.com>
2022-07-25 14:01:27 -0400

    added response time for auth plugins to metrics for prometheus

--- turnpike/metrics.py
+++ turnpike/metrics.py
@@ -1,5 +1,9 @@
-from prometheus_client import Counter
+from prometheus_client import Counter, Histogram
 
 
 class Metrics:
     request_count = Counter("requests", "Request Count", ["service", "policy_status_code"])
+
+
+class AuthMetrics:
+    auth_request_latency = Histogram(name="request_latency_ms", documentation="Response time for requests")

3ac41c6dbeafa2afcd98032c150a297f08668207
Matt Holder <mjholder96@gmail.com>
2022-07-28 10:26:04 -0400

    added new metrics to track

--- turnpike/metrics.py
+++ turnpike/metrics.py
@@ -3,7 +3,9 @@ from prometheus_client import Counter, Histogram
 
 class Metrics:
     request_count = Counter("requests", "Request Count", ["service", "policy_status_code"])
+    request_latency = Histogram("request_latency", "Latency of Requests", ["service"])
 
 
 class AuthMetrics:
-    auth_request_latency = Histogram(name="request_latency_ms", documentation="Response time for requests", labelnames=["auth_type"])
+    auth_request_latency = Histogram("auth_request_latency", "Response time for requests", ["auth_type"])
+    auth_request_status = Counter("auth_request_status", "Response status code for auth plugins", ["auth_type", "status_code"])

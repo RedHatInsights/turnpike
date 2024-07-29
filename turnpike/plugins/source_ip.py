b87bf3fb57977f4bd2247a837a82a30efdd1891e
Jay Zeng <astrozzc@gmail.com>
2024-07-29 09:17:27 -0400

    Revert "Temporary log for debug source ip plugin"
    This reverts commit cde7f96484d57b5b09b29894a8e5408db82ed187.

--- turnpike/plugins/source_ip.py
+++ turnpike/plugins/source_ip.py
@@ -7,9 +7,6 @@ from ..plugin import TurnpikePlugin
 
 class SourceIPPlugin(TurnpikePlugin):
     def process(self, context):
-        # Temporary log for debugging
-        if context.backend["name"] in ("rhel-ai", "pulp-rubygems-cache"):
-            current_app.logger.debug(context.backend)
         if "source_ip" not in context.backend:
             return context
 

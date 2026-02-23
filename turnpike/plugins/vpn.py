9b214a2f5f8aff26674f186a3465955ab4d5fcd2
Daniel Agbay <101584191+dagbay-rh@users.noreply.github.com>
2026-02-23 14:48:56 -0600

    skip vpn plugin for public backends (#596)
    * entirely skip vpn plubin when backend is not private
    
    * fix typo
    
    * change var name

--- turnpike/plugins/vpn.py
+++ turnpike/plugins/vpn.py
@@ -27,9 +27,15 @@ class VPNPlugin(TurnpikePlugin):
         backend_name = context.backend["name"]
 
         # Determine whether the backend is VPN-restricted or not.
-        vpn_edge_host_header_required: bool = (self.vpn_config_key in context.backend) and (
+        is_backend_private: bool = (self.vpn_config_key in context.backend) and (
             context.backend.get(self.vpn_config_key) == True
         )
+
+        if not is_backend_private:
+            self.app.logger.debug(
+                f'[backend: "{backend_name}"][{HeaderValidator.EDGE_HOST_HEADER}: "{edge_host}"] VPN plugin skipped. Backend is not VPN restricted'
+            )
+            return context
 
         # When the "edge host" header is not present for VPN-restricted back
         # ends, the request needs to be rejected.
@@ -37,17 +43,13 @@ class VPNPlugin(TurnpikePlugin):
         # Otherwise, it is safe to skip the VPN plug in since the back end is
         # considered to be public.
         if not edge_host:
-            if vpn_edge_host_header_required:
-                # TODO: integrate glitchtip with turnpike and capture this so we get alert if it happens, see https://issues.redhat.com/browse/RHCLOUD-40788
-                self.app.logger.info(
-                    f'[backend: "{backend_name}"] Request denied. Missing mandatory "{HeaderValidator.EDGE_HOST_HEADER}" header for VPN restricted backend'
-                )
+            # TODO: integrate glitchtip with turnpike and capture this so we get alert if it happens, see https://issues.redhat.com/browse/RHCLOUD-40788
+            self.app.logger.info(
+                f'[backend: "{backend_name}"] Request denied. Missing mandatory "{HeaderValidator.EDGE_HOST_HEADER}" header for VPN restricted backend'
+            )
 
-                context.status_code = HTTPStatus.FORBIDDEN
-                return context
-            else:
-                self.app.logger.debug(f'[backend: "{backend_name}"] VPN plugin skipped. Backend is not VPN restricted')
-                return context
+            context.status_code = HTTPStatus.FORBIDDEN
+            return context
 
         # Make sure that the "edge host" header is valid.
         try:
@@ -69,19 +71,13 @@ class VPNPlugin(TurnpikePlugin):
         # When it is, we need to reject the request. Otherwise, the VPN plugin
         # needs to be skipped so that we don't set the Nginx header flag
         # indicating that the origin of the request is the private network.
-        if network == AllowedNetworks.INTERNAL:
-            if vpn_edge_host_header_required:
-                self.app.logger.info(
-                    f'[backend: "{backend_name}"][{HeaderValidator.EDGE_HOST_HEADER}: "{edge_host}"] Request denied. Backend requires the requests to come from the VPN'
-                )
+        if network != AllowedNetworks.PRIVATE:
+            self.app.logger.info(
+                f'[backend: "{backend_name}"][{HeaderValidator.EDGE_HOST_HEADER}: "{edge_host}"] Request denied. Backend requires the requests to come from the VPN'
+            )
 
-                context.status_code = HTTPStatus.FORBIDDEN
-                return context
-            else:
-                self.app.logger.debug(
-                    f'[backend: "{backend_name}"][{HeaderValidator.EDGE_HOST_HEADER}: "{edge_host}"] VPN plugin skipped. Backend is not VPN restricted'
-                )
-                return context
+            context.status_code = HTTPStatus.FORBIDDEN
+            return context
 
         self.app.logger.debug(
             f'[backend: "{backend_name}"][{HeaderValidator.EDGE_HOST_HEADER}: "{edge_host}"] Request successfully passed through the VPN plugin'

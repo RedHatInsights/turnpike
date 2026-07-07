2f567d0acf25d8913572cadfe894cddae9f7e23a
EvanCasey13 <evancasey2003@gmail.com>
2026-07-07 11:02:54 +0100

    use python 3.12 minimal and update konflux pr check sh script

--- konflux-pr-check.sh
+++ konflux-pr-check.sh
@@ -2,20 +2,20 @@
 set -ex
 
 echo "INSTALL DEPENDENCIES"
-microdnf install --nodocs -y gcc python311 python3.11-pip python3-devel xmlsec1
-python3.11 -m pip install --upgrade pip
-python3.11 -m pip install micropipenv
+microdnf install --nodocs -y gcc python3-devel xmlsec1
+python3 -m pip install --upgrade pip
+python3 -m pip install micropipenv
 micropipenv install
 
 # #Run black/lint command
-python3.11 -m pip install black==26.1.0
+python3 -m pip install black==26.1.0
 if ! (black --check -l 119 -t py311 /var/workdir --diff); then
     echo "black formatter encountered an issue"
     exit 1
 fi
 
 echo "Run the unit tests"
-python3.11 -m pip install pytest
+python3 -m pip install pytest
 echo "RUN THE UNIT TESTS"
 pytest --disable-pytest-warnings tests/
 result=$?
